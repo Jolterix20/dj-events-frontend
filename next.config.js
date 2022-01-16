82811ddfc16e049c97de31b0f33fcda74c322560
Jolterix20 <jayeshvbane@gmail.com>
2022-01-16 12:01:08 +0530

    updated config

--- next.config.js
+++ next.config.js
@@ -3,9 +3,4 @@ module.exports = {
 	images: {
 		domains: ['res.cloudinary.com'],
 	},
-	env: {
-		NEXT_PUBLIC_MAPBOX_API_TOKEN:
-			'pk.eyJ1Ijoiam9sdGVyaXgyMCIsImEiOiJja3llYnBwamowYW5iMm9zOGkwZ20yNmRwIn0.zoZcFha3C8h0h0CSC_3qGQ',
-		NEXT_PUBLIC_MAPQUEST_API_KEY: 'opumL3QBhinShuuXtC0g5S9pt65tjLV0',
-	},
 }
